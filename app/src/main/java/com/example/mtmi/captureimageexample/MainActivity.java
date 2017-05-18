f26184c4af4dd5228fb8f46156ef5840ba2d3ed9
glsmygt <gulcanyigit@marun.edu.tr>
2017-05-19 00:43:54 +0300

    creating test branch

--- app/src/main/java/com/example/mtmi/captureimageexample/MainActivity.java
+++ app/src/main/java/com/example/mtmi/captureimageexample/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.mtmi.captureimageexample;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +16,7 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE=1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
     Button take_picture_btn;
     ImageView picturePreview;
 
@@ -20,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        take_picture_btn= (Button) findViewById(R.id.take_picture_btn);
+        take_picture_btn = (Button) findViewById(R.id.take_picture_btn);
 
         take_picture_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
                 take_picture_btn.setVisibility(View.GONE);
 
-                Intent takePictureIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if(takePictureIntent.resolveActivity(getPackageManager())!=null){
-                    startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
                 }
             }
         });
@@ -39,14 +44,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        picturePreview= (ImageView) findViewById(R.id.picturePreview);
+        picturePreview = (ImageView) findViewById(R.id.picturePreview);
 
-        if(requestCode==REQUEST_IMAGE_CAPTURE&&resultCode==RESULT_OK){
-            Bundle extras=data.getExtras();
-            Bitmap imageBitmap= (Bitmap) extras.get("data");
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = writeTextBitmap(imageBitmap);
             picturePreview.setImageBitmap(imageBitmap);
             picturePreview.setVisibility(View.GONE);
             picturePreview.setVisibility(View.VISIBLE);
         }
+    }
+
+    private Bitmap writeTextBitmap(Bitmap bitmap) {
+
+        Bitmap dest = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Typeface tf = Typeface.create("Helvetica", Typeface.BOLD);
+
+        String text = "hello world";
+
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.GREEN);
+        paint.setTypeface(tf);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setTextSize(dest.getHeight() * .04629f);
+
+        Rect textRect = new Rect();
+        paint.getTextBounds(text, 0, text.length(), textRect);
+
+        Canvas canvas = new Canvas(dest);
+
+//        //If the text is bigger than the canvas , reduce the font size
+//        if(textRect.width() >= (canvas.getWidth() - 4))     //the padding on either sides is considered as 4, so as to appropriately fit in the text
+//            paint.setTextSize(convertToPixels(mContext, 7));        //Scaling needs to be used for different dpi's
+
+        //Calculate the positions
+        int xPos = (canvas.getWidth() / 2) - 2;     //-2 is for regulating the x position offset
+
+        //"- ((paint.descent() + paint.ascent()) / 2)" is the distance from the baseline to the center.
+        int yPos = (int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2));
+
+        canvas.drawText(text, xPos, yPos, paint);
+
+        return dest;
     }
 }
